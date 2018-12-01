1f2aa5b1457b1c2426c8076afc4cb171f6ff77bf
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-12-01 18:43:56 +0900

    #3 Add とりあえず中心に描画

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -6,6 +6,40 @@
 export default {
   props: {
     inputTexts: ''
+  },
+  watch: {
+    inputTexts () {
+      this.draw(this.inputTexts)
+    }
+  },
+  methods: {
+    draw (texts) {
+      const context = this.ctx
+      const canvasWidth = 400
+      const canvasHeight = 300
+      const fontSize = 36
+
+      // 初期化
+      context.clearRect(0, 0, canvasWidth, canvasHeight)
+      // 背景色を黒にして塗りつぶす
+      context.fillStyle = '#000'
+      context.fillRect(0, 0, canvasWidth, canvasHeight)
+
+      // フォント設定
+      context.textAlign = 'center'
+      context.font = 'bold ' + fontSize + 'px Arial, meiryo, sans-serif'
+      context.fillStyle = '#fff'
+
+      // 書きだす
+      context.beginPath()
+
+      // 描画
+      context.fillText(texts, canvasWidth / 2, canvasHeight / 2)
+    }
+  },
+  mounted () {
+    this.ctx = this.$el.getContext('2d')
+    this.draw(this.inputTexts)
   }
 }
 </script>
