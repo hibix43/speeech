8ba2272a890cc56c482ce408e0153e5abcfd68af
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-12-01 19:34:20 +0900

    #6 Add DataManagerのアウトラインを追加

---
+++ src/components/DataManager.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>{{ Slides }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    inputTexts: ''
+  },
+  data () {
+    return {
+      Slides: []
+    }
+  },
+  methods: {
+    // getter
+    getSlideTexts (num) {
+      return this.Slides[num]
+    },
+    // setter
+    setSlideTexts (mum) {
+    },
+    // 変更
+    changeSlideTexts (num) {
+    },
+    // 追加
+    addNewSlide () {
+    },
+  }
+}
+</script>
