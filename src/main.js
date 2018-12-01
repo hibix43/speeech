a47b4b36459dd842ecb0e2e67cf3b7cbdac39926
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-12-01 17:42:44 +0900

    #1 Add index.htmlとIndex.vueを結びつける

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Index from './components/Index'
 // import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+new Vue({
+  el: '#index',
+  component: { Index },
+  template: '<Index/>'
+})
