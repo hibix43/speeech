cbfc00d693093ac2b9aaa5325fc8832343f8fab8
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-12-01 19:41:44 +0900

    #6 Add Indexにアウトラインを追加

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="index">
+    <DataManager :inputTexts="texts" ref="manager"></DataManager>
     <Canvas :inputTexts="texts"></Canvas><br>
     <textarea v-model="texts" placeholder="input here"/><br>
+    <button type="submit" @click="prevSlide">前のスライドへ</button>
+    <button type="submit" @click="nextSlide">次のスライドへ</button>
+    <!--<input type="range" min="0" v-bind:max="slideLength" step="1" v-model="number">-->
     <p>texts: {{ texts }}</p>
+    <p>slideNum: {{ slideNum }}</p>
+    <p>slideLength: {{ slideLength }}</p>
   </div>
 </template>
 
 <script>
+import DataManager from './DataManager.vue'
 import Canvas from './Canvas.vue'
 export default {
   components: {
+    DataManager,
     Canvas
   },
   data () {
     return {
-      texts: ''
+      texts: '',
+      slideNum: 0,
+      slideLength: 0
+    }
+  },
+  methods: {
+    nextSlide () {
+    },
+    prevSlide () {
     }
   }
 }
