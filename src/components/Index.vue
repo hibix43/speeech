e01a4a82012c20483f4c2af8e00b1868a0cc2179
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-12-08 23:32:11 +0900

    #10 Add ボタンデザイン

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -4,15 +4,15 @@
     <Canvas :inputTexts="texts" ref="canvas"></Canvas>
     <textarea v-model="texts" placeholder="input here"/>
     <div class="row">
-      <button class="six columns" type="submit" @click="prevSlide">前のスライドへ</button>
-      <button class="six columns" type="submit" @click="nextSlide">次のスライドへ</button>
+      <button class="btn six columns" type="submit" @click="prevSlide">前のスライドへ</button>
+      <button class="btn six columns" type="submit" @click="nextSlide">次のスライドへ</button>
     </div>
     <div class="row">
-      <button class="four columns" type="submit" @click="startAnimation(0)">最初のスライドから再生</button>
-      <button class="four columns" type="submit" @click="startAnimation(index)">現在のスライドから再生</button>
-      <button class="four columns" type="submit" @click="stopAnimation">停止</button>
+      <button class="btn four columns" type="submit" @click="startAnimation(0)">最初のスライドから再生</button>
+      <button class="btn four columns" type="submit" @click="startAnimation(index)">現在のスライドから再生</button>
+      <button class="btn four columns" type="submit" @click="stopAnimation">停止</button>
     </div>
-    <button type="submit">完成!!</button>
+    <button class="btn complete-btn" type="submit">完成させてツイートする</button>
   </div>
 </template>
 
@@ -68,6 +68,23 @@ textarea {
   margin: 16px 0;
 }
 .row {
-  margin: 0 64px;
+  margin: 16px 64px;
+}
+.btn {
+  font-size: 1em;
+  font-weight: bold;
+  background-color: #666;
+  color: #fff;
+}
+.complete-btn {
+  font-size: 1.5em;
+  border-radius: 0;
+  margin: 8px 0;
+  width: 320px;
+  height: 60px;
+  background-color: #00bbee;
+  color: #fff;
+  border: 1px solid #fff;
+  box-shadow: 0 0 4px #00bbee;
 }
 </style>
