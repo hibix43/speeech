b98aab6ec83675514784ed2d060512827155ca61
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-12-22 00:03:12 +0900

    #23 Fix JSONにキーがない場合の処理

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, jsonify, request
 import twitter
 
 app = Flask(__name__,
-            static_folder="../dist/static",
-            template_folder="../dist")
+            static_folder='../dist/static',
+            template_folder='../dist')
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @app.route('/tweet', methods=['GET', 'POST'])
@@ -17,7 +17,7 @@ def tweet_with_img():
     if request.method == 'POST':
         json = request.get_json()
         img_url = json['img']
-        message = json['msg']
+        message = json['msg'] if 'msg' in json.keys() else ''
         response = twitter.upload(img_url)
         if 'media_ids' in response.keys():
             response['status'] = message + ' #speeech'
